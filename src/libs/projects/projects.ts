51d1a07b2569a2b9ee93ec1408a8c4d65deb75c4
mich43ll <michaell@gmail.com>
2023-05-27 17:40:39 -0600

    no se

---
+++ src/libs/projects/projects.ts
@@ -0,0 +1,26 @@
+export interface IProject{
+    _id?: string;
+    name: string;
+    description: string;
+    isActive: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+//const newPrject: Required<IProject>={}; hace que cada uno de los campos de Iproject sean requeridos
+const memoryProjects: IProject[] = [];
+let createdProjects = 0;
+
+export const createProject = async (project: IProject) => {
+    const newProject = {...project};
+    newProject._id = (++createdProjects).toString();
+    newProject.createdAt = new Date();
+    newProject.updatedAt = newProject.createdAt;
+    memoryProjects.push(newProject);
+    return newProject;
+}
+
+export const getProjects = async{} => {
+    return memoryProjects;
+    
+}
--- src/routes/api/projects/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', (_req, res) =>{
-    res.json({version:1, scope: 'projects'})
-});
-
-export default router;
